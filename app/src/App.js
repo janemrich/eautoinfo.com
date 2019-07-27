526ddff69604e10f7957dfa05ad19d3aae603678
Frederik Wegner <onlyknoppsa@gmail.com>
2019-07-27 20:11:54 +0200

    moved strapi api into a separate folder

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CarsList } from './Cars.js';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+class App extends Component {
+	
+	cars = []
+
+	componentDidMount() {
+        fetch('./cars')
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ cars: data })
+        })
+		.catch(console.log);
+		alert("fetched");
+    }
+
+	render() {
+		return (
+			<div>
+			<h1>Hallo Einfo</h1>
+			<div className="App">
+				<CarsList cars={this.cars} />
+			</div>
+			</div>
+		);
+	}
+
 }
 
 export default App;
---
+++ app/src/Cars.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+function CarCard(props) {
+	return (
+		<div>
+			<img src={props.car.thumbnail.url} />
+			<p>{props.car.manufacturer} {props.car.model} {props.car.edition}</p>
+		</div>
+	);
+}
+
+export function CarsList(props) {
+	const carsCards = props.cars.map(car => <CarCard car={car}/>);
+	return (
+		<div>
+			{carsCards}
+		</div>
+	);
+}

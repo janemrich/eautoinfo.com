13ce7c52e038a2ebcbc3bcda7c0911089ac8c844
JazzyJun <z.chen2@student.tue.nl>
2020-02-20 17:48:36 +0100

    code alignment corrected

--- app/src/App.js
+++ app/src/App.js
@@ -180,19 +180,19 @@ class App extends Component {
 		return (
 				<div className="App">
 					<Bar onClick={ () => this.handleMainClick()}/>
-						<sortbyContext.Provider value = {this.state.sortby}>
-							<Sort
-								onPriceChange={ this.handlePriceChange }
-								price={this.state.price}
-								// onRangeChange={ this.handleRangeChange }
-								range={this.state.range}
-								sortby={this.state.sortby}
-								onSortChange={(type) => this.handleSortChange(type)}
-								brands={ this.getBrands() }
-								filter_brands={ this.state.filter_brands }
-								onBrandChange={(brand) => this.handleBrandChange(brand)}
-							/>
-						</sortbyContext.Provider>
+					<sortbyContext.Provider value = {this.state.sortby}>
+						<Sort
+							onPriceChange={ this.handlePriceChange }
+							price={this.state.price}
+							// onRangeChange={ this.handleRangeChange }
+							range={this.state.range}
+							sortby={this.state.sortby}
+							onSortChange={(type) => this.handleSortChange(type)}
+							brands={ this.getBrands() }
+							filter_brands={ this.state.filter_brands }
+							onBrandChange={(brand) => this.handleBrandChange(brand)}
+						/>
+					</sortbyContext.Provider>
 					
 					<CarsList
 						cars={ this.selectCarList(this.state.cars) }

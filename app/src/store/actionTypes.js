7910062afd2baf370c0ce140be0313ddda6954b4
JazzyJun <z.chen2@student.tue.nl>
2020-03-22 14:50:45 +0100

    Create actionTypes.js

---
+++ app/src/store/actionTypes.js
@@ -0,0 +1,7 @@
+const CHANGE_SORTBY_VALUE = 'change_sortby_value';
+const CHANGE_PRICE_VALUE = 'change_price_value';
+const CHANGE_RANGE_VALUE = 'change_range_value';
+const CHANGE_FILTERBRAND_VALUE = 'change_filterbrands_value';
+const CHANGE_CARS_VALUE = 'change_cars_value';
+const CHANGE_TODETAIL_VALUE = 'change_todetail_value';
+const CHANGE_CARSTOCOMPARE_VALUE = 'change_carstocompare_value';

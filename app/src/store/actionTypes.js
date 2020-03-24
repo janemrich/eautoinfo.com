a800be0ffa79fdec4cc12f0e53fcb76853a32d83
JazzyJun <z.chen2@student.tue.nl>
2020-03-24 15:09:40 +0100

    create action for componentDidMount()

--- app/src/store/actionTypes.js
+++ app/src/store/actionTypes.js
@@ -2,6 +2,6 @@ const CHANGE_SORTBY_VALUE = 'change_sortby_value';
 const CHANGE_PRICE_VALUE = 'change_price_value';
 const CHANGE_RANGE_VALUE = 'change_range_value';
 const CHANGE_FILTERBRAND_VALUE = 'change_filterbrands_value';
-const CHANGE_CARS_VALUE = 'change_cars_value';
+const INIT_CARS_VALUE = 'init_cars_value';
 const CHANGE_TODETAIL_VALUE = 'change_todetail_value';
 const CHANGE_CARSTOCOMPARE_VALUE = 'change_carstocompare_value';
--- app/src/store/reducer.js
+++ app/src/store/reducer.js
@@ -1,3 +1,4 @@
+import {CHANGE_SORTBY_VALUE, CHANGE_PRICE_VALUE, CHANGE_RANGE_VALUE, CHANGE_FILTERBRAND_VALUE, INIT_CARS_VALUE, CHANGE_TODETAIL_VALUE, CHANGE_CARSTOCOMPARE_VALUE} from './actionTypes';
 const defaultState = {
   display: "Not OK",
   cars: [],
@@ -13,6 +14,11 @@ export default (state = defaultState, action) => {
   if (action.type === 'change_input_value') {
     const newState = JSON.parse(JSON.stringify(state));
     newState.display = action.value;
+    return newState;
+  }
+  if (action.type === INIT_CARS_VALUE) {
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.cars = action.data;
     return newState;
   }
   console.log(state, action);

5bab558854eb7fed7f7e08d3acd982bd750d3996
JazzyJun <z.chen2@student.tue.nl>
2020-03-24 15:02:07 +0100

    add redux-devtools-extension for debugging

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
-
+const { composeWithDevTools } = require('redux-devtools-extension');
 const store = createStore(
   reducer
+  , composeWithDevTools()
   // +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
